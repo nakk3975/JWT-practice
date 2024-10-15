ece5093a4b962be998f58f41018a33da11925c6b
안재원 <dsfs3975@naver.com>
2024-10-15 19:40:32 +0900

    [안재원] 2024-10-15  - mysql workbench > DBeaver로 변경  - MicroSoft Azure 호스팅 받기 및 연결  - DB연결 확인  - JPA 설치  - JPA로 테이블 생성

--- src/main/java/com/ahn/shop/Item.java
+++ src/main/java/com/ahn/shop/Item.java
@@ -1,4 +1,14 @@
 package com.ahn.shop;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    Integer price;
 }
